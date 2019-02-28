9cdd42abf1b9c8e4c25674b645a7fae52223121f
jylong0909 <37680024+jylong0909@users.noreply.github.com>
2019-02-28 10:12:26 +0800

    Add files via upload

---
+++ static/ipConfig.js
@@ -0,0 +1,29 @@
+// 测试开发ip
+const ipConfigDefault = "192.168.1.9:4399"
+const ipConfigFace = "192.168.1.172:5555"
+const ipConfigCar = "192.168.1.9:5555"
+const ipConfigAccessControl = "192.168.1.144:8080"
+const faceSrcIdList = {
+  '7d7e37ab-2b58-4ac8-a45f-29703a48f7dd': 0,
+  '66d046f8-503e-450d-aede-8f55e3c5bf47': 1
+}
+const faceSrcIdParams = '?src_in=66d046f8-503e-450d-aede-8f55e3c5bf47&src_out='
+
+// 正式ip
+// const ipConfigDefault = ''
+// const ipConfigFace = '192.168.1.209:5555'
+// const ipConfigCar = '192.168.1.208:8888'
+// const ipConfigAccessControl = '192.168.1.209:5555'
+
+// 公网ip
+// const ipConfigDefault = ''
+// const ipConfigFace = '111.17.205.190:5555'
+// const ipConfigCar = '111.17.205.190:8888'
+// const ipConfigAccessControl = '111.17.205.190:5555'
+// const faceSrcIdList = {
+//   '0e1ebc86-4d52-4341-aaf1-162489fa2ccb': 0,
+//   'dcbfbb31-c653-497b-a4e0-ddc38cef269a': 0,
+//   '64ca5f06-f021-4e50-b0a8-46baf2ba6479': 1,
+//   'a22dfc57-8e4a-4e46-906b-60eaf24d1b9b': 1
+// }
+// const faceSrcIdParams = '?src_in=0e1ebc86-4d52-4341-aaf1-162489fa2ccb,dcbfbb31-c653-497b-a4e0-ddc38cef269a&src_out=64ca5f06-f021-4e50-b0a8-46baf2ba6479,a22dfc57-8e4a-4e46-906b-60eaf24d1b9b'
